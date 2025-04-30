1d2b226c8249ff0963d1499d59b38b8545b847d8
Gael Borchardt <gaelborchardt@gmail.com>
2025-04-30 16:28:00 -0700

    app tsx h2

--- ci-cd/src/App.tsx
+++ ci-cd/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>wasap hola asdasdasfadsad</h1>
+      <h2>saludos ........ ..... ....</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
